5342f6ac44e045981b44bb15478169e764d185cc
kerenmj <kerentehua0@gmail.com>
2024-06-12 12:14:55 +0000

    Add files via upload

---
+++ SOMME 1.py
@@ -0,0 +1,6 @@
+chiffre1 = 3
+chiffre2 = 3
+
+total = chiffre1 + chiffre2
+
+print("la somme est: ", total)
---
+++ SOMME 2.py
@@ -0,0 +1,6 @@
+chiffre1 = int(input("donne le chiffre 1 : "))
+chiffre2 = int(input("donne le chiffre 2 : "))
+
+total = chiffre1 + chiffre2
+
+print("la somme est: ", total)
